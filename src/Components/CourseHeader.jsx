6967dccb19a85216c992fc21186a6eef5ec543de
adharsh2000 <adharshmr001@gmail.com>
2023-09-12 18:49:35 +0530

    course header completed

---
+++ src/Components/CourseHeader.jsx
@@ -0,0 +1,18 @@
+import Arrow from "../assets/CurleyArrow.svg";
+
+const CourseHeader = () => {
+  return (
+    <div className="flex items-center justify-center relative mb-5">
+      <div>
+        <h2 className="font-bold text-3xl text-center mb-5">Our Courses</h2>
+        <p className="text-gray-500 mx-auto w-[55%]">
+          Lorem Ipsum has been the industry's standard dummy text ever since the
+          when an unknown printer took a galley of type and scrambled it to make
+        </p>
+      </div>
+      <img src={Arrow} alt="arrow" className="inline-block absolute right-2" />
+    </div>
+  );
+};
+
+export default CourseHeader;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import CourseHeader from "../Components/CourseHeader"
 import Hero from "../Components/Hero"
 import Navbar from "../Components/Navbar"
 
@@ -6,6 +7,7 @@ const Home = () => {
     <>
         <Navbar />
         <Hero />
+        <CourseHeader />
     </>
   )
 }
