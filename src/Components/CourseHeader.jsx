42efff7f09dd18f84970bc6cf38c14ff2e49ec84
adharsh2000 <adharshmr001@gmail.com>
2023-09-13 10:12:02 +0530

    exp card

--- src/Components/CourseHeader.jsx
+++ src/Components/CourseHeader.jsx
@@ -5,7 +5,7 @@ const CourseHeader = () => {
     <div className="flex items-center justify-center relative mb-5">
       <div>
         <h2 className="font-bold text-3xl text-center mb-5">Our Courses</h2>
-        <p className="text-gray-500 mx-auto w-[55%]">
+        <p className="text-gray-500 mx-auto w-[55%] text-center">
           Lorem Ipsum has been the industry's standard dummy text ever since the
           when an unknown printer took a galley of type and scrambled it to make
         </p>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,29 +1,71 @@
+import { Fade } from "react-reveal";
 import CourseCard from "../Components/CourseCard";
 import CourseHeader from "../Components/CourseHeader";
 import Hero from "../Components/Hero";
 import Navbar from "../Components/Navbar";
 import GraphicDesign from "../assets/C1.svg";
-
+import ExpPremium from "../Components/ExpPremium";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <Hero />
-      <CourseHeader />
+      {/* <Fade bottom> */}
+        <Hero />
+      {/* </Fade> */}
+      <Fade bottom>
+        <CourseHeader />
+      </Fade>
       <div className="container mb-10 mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <CourseCard Heading={"Graphic Design"} content={"Photoshop,illustrator,indesign"} img={GraphicDesign} />
-          <CourseCard Heading={"Graphic Design"} content={"Photoshop,illustrator,indesign"} img={GraphicDesign} />
-          <CourseCard Heading={"Graphic Design"} content={"Photoshop,illustrator,indesign"} img={GraphicDesign} />
-          <CourseCard Heading={"Graphic Design"} content={"Photoshop,illustrator,indesign"} img={GraphicDesign} />
-          <CourseCard Heading={"Graphic Design"} content={"Photoshop,illustrator,indesign"} img={GraphicDesign} />
-          <CourseCard Heading={"Graphic Design"} content={"Photoshop,illustrator,indesign"} img={GraphicDesign} />
-          <CourseCard Heading={"Graphic Design"} content={"Photoshop,illustrator,indesign"} img={GraphicDesign} />
-          <CourseCard Heading={"Graphic Design"} content={"Photoshop,illustrator,indesign"} img={GraphicDesign} />
-          <CourseCard Heading={"Graphic Design"} content={"Photoshop,illustrator,indesign"} img={GraphicDesign} />
+          <CourseCard
+            Heading={"Graphic Design"}
+            content={"Photoshop,illustrator,indesign"}
+            img={GraphicDesign}
+          />
+          <CourseCard
+            Heading={"Graphic Design"}
+            content={"Photoshop,illustrator,indesign"}
+            img={GraphicDesign}
+          />
+          <CourseCard
+            Heading={"Graphic Design"}
+            content={"Photoshop,illustrator,indesign"}
+            img={GraphicDesign}
+          />
+          <CourseCard
+            Heading={"Graphic Design"}
+            content={"Photoshop,illustrator,indesign"}
+            img={GraphicDesign}
+          />
+          <CourseCard
+            Heading={"Graphic Design"}
+            content={"Photoshop,illustrator,indesign"}
+            img={GraphicDesign}
+          />
+          <CourseCard
+            Heading={"Graphic Design"}
+            content={"Photoshop,illustrator,indesign"}
+            img={GraphicDesign}
+          />
+          <CourseCard
+            Heading={"Graphic Design"}
+            content={"Photoshop,illustrator,indesign"}
+            img={GraphicDesign}
+          />
+          <CourseCard
+            Heading={"Graphic Design"}
+            content={"Photoshop,illustrator,indesign"}
+            img={GraphicDesign}
+          />
+          <CourseCard
+            Heading={"Graphic Design"}
+            content={"Photoshop,illustrator,indesign"}
+            img={GraphicDesign}
+          />
         </div>
       </div>
+      <ExpPremium />
     </>
   );
 };
