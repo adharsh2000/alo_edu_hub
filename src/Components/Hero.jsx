4944702447fd801f47e31ae82c2ba66c5af5749c
adharsh2000 <adharshmr001@gmail.com>
2023-09-14 19:30:02 +0530

    hero card bug fixed

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <div className="mb-10">
-      <main className="md:h-[550px] flex flex-col md:flex-row">
+      <main className="md:h-[550px] flex flex-col-reverse md:flex-row">
         {/* <img src={HeroImg} alt="" /> */}
         <div
           style={{ backgroundImage: `url(${CircularOne})` }}
@@ -37,7 +37,7 @@ const Hero = () => {
             </p>
           </Fade>
           <Link to="/getstarted">
-            <button className="bg-app-color ml-10 mt-4 text-white p-3 rounded-md">
+            <button className="bg-app-color ml-10 mt-4 mb-10 md:mb-0 text-white p-3 rounded-md">
               Get Started
             </button>
           </Link>
@@ -48,30 +48,30 @@ const Hero = () => {
           </div>
         </Fade>
       </main>
-      <div className="bg-app-color w-[90%] md:w-[90%] mx-auto p-4 rounded-lg flex flex-col md:flex-row  gap-4">
+      <div className="bg-app-color w-[90%] md:w-[90%] md:h-36 md:p-5 mx-auto p-4 rounded-lg flex flex-col md:flex-row  gap-4">
         <Fade bottom>
         <div className="flex flex-col gap-3 text-center justify-around md:justify-between md:text-start">
-          <h2 className="text-white font-bold md:text-2xl">Why choose Us</h2>
-          <p className="text-white text-xs md:text-sm">
+          <h2 className="text-white font-bold md:text-4xl">Why choose Us</h2>
+          <p className="text-white text-xs md:text-lg text-center md:text-start">
             Lorem ipsum dolor sit amet, erat porttitor eget. Vestibulum quis
             orci lacus. Sed in neque in nisi
           </p>
         </div>
         <div className="flex flex-col gap-3 items-center text-center justify-around md:items-center md:justify-between">
           <img src={BookIcon} alt="book" className="w-10" />
-          <p className="text-white text-xs md:text-sm">
+          <p className="text-white text-md md:text-lg">
             Practical experience with a live project.
           </p>
         </div>
         <div className="flex flex-col gap-3 items-center text-center justify-around md:items-center md:justify-between">
           <img src={Clock} alt="clock" className="w-10" />
-          <p className="text-white text-xs md:text-sm">
+          <p className="text-white text-md md:text-lg">
             Flexible timings to suit the needs of the students.
           </p>
         </div>
         <div className="flex flex-col gap-3 items-center text-center justify-around md:items-center md:justify-between">
           <img src={Avatars} alt="avatar" className="w-10" />
-          <p className="text-white text-xs md:text-sm">
+          <p className="text-white text-md md:text-lg">
             Training provided by well-experienced trainers.
           </p>
         </div>
